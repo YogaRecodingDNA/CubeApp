fa065174255efedd6f093fabcd287b29004cb042
Chadd <74035551+YogaRecodingDNA@users.noreply.github.com>
2021-08-27 15:15:32 -0700

    Validation and Error Handling

--- config/express.js
+++ config/express.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
     
-    // View Engine Setup
+    // View Engine
     app.set("view engine", "hbs");
     app.engine(
 	"hbs",
@@ -14,33 +14,16 @@ module.exports = (app) => {
 		defaultLayout: "",
         layoutsDir: __dirname + "/views",
         partialsDir: __dirname + "/views",
-	})
-);
+	}));
     
-    //TODO: Setup the body parser
+    
+    // Body Parser
     app.use(bodyParser.urlencoded({extended: true}));
 
-    // app.use(jwt());
+    // Cookie Parser
     app.use(cookieParser());
 
-    //TODO: Setup the static files
+    // Static Files
     app.use(express.static('static'));
 
-
 };
-
-
-
-
-
-
-
-// app.set('views', path.join(__dirname, 'views')); // setting folder for public files
-
-    // // register the partials, hint if it says module not found after you do this, its because the module most likely isn't there! Import it! 
-
-    // hbs.registerPartials(__dirname + '/views/partials'); 
-
-    // // setting view engine to hbs, engine compiles views and data into HTML
-
-    // app.set('view engine', 'hbs');
